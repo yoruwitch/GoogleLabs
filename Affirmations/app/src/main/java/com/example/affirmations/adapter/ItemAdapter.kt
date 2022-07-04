5e65cbac591f287ea4bbf8aa2dea4ea836416d08
yoruwitch <evelyn.fernandes4@gmail.com>
2022-07-04 17:02:44 -0300

    FEAT: Images showing up with affirmations

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
-/**
- * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
- */
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>
+
+// Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+
+class ItemAdapter(
+    private val context: Context, private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     // Provide a reference to the views for each data item
@@ -21,11 +23,11 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
-    /**
-     * Create new views (invoked by the layout manager)
-     */
+    // Create new views (invoked by the layout manager)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
 
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -33,16 +35,17 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
         return ItemViewHolder(adapterLayout)
     }
 
-    /**
-     * Replace the contents of a view (invoked by the layout manager)
-     */
+
+     // Replace the contents of a view (invoked by the layout manager)
+
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
-    /**
-     * Return the size of your dataset (invoked by the layout manager)
-     */
+
+     // Return the size of your dataset (invoked by the layout manager)
+
     override fun getItemCount() = dataset.size
 }
